7720842d205ecf1ef400a716c97c8a4b71806c1f
Davide Basile <davide.basile01@gmail.com>
2019-04-03 17:03:42 +0200

    updated PublicRestController

--- src/main/java/it/unisalento/db/project/controllers/rest/PublicRestController.java
+++ src/main/java/it/unisalento/db/project/controllers/rest/PublicRestController.java
@@ -1,9 +1,12 @@
 package it.unisalento.db.project.controllers.rest;
 
+import it.unisalento.db.project.models.dto.GlassdoorJobDetails;
 import it.unisalento.db.project.models.dto.MonsterJobDetails;
+import it.unisalento.db.project.services.GlassdoorParserService;
 import it.unisalento.db.project.services.LinkedinParserService;
 import it.unisalento.db.project.services.MonsterParserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.AutoConfigureOrder;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -21,6 +24,9 @@ public class PublicRestController {
     @Autowired
     private MonsterParserService monsterParserService;
 
+    @Autowired
+    private GlassdoorParserService glassdoorParserService;
+
     @PostMapping(value = "/linkedin", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     public Map<String, String> parseLinkedInLink(@RequestParam("url") String url) throws IOException {
         return linkedinParserService.parse(url);
@@ -29,6 +35,11 @@ public class PublicRestController {
     @PostMapping(value = "/monster", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     public MonsterJobDetails parseMonsterLink(@RequestParam("url") String url) throws IOException {
         return monsterParserService.parse(url);
+    }
+
+    @PostMapping(value = "/glassdoor", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public GlassdoorJobDetails parseGlassdoorLink(@RequestParam("url") String url) throws IOException {
+        return glassdoorParserService.parse(url);
     }
 
 }
