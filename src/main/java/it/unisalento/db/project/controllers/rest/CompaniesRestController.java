062a52d02c1dd53b5b3a4d6761685678c1ad332a
Leonardo <leonardo.capoccia@hotmail.it>
2019-04-24 16:18:58 +0200

    Add company dto and add service to get company from ID

--- src/main/java/it/unisalento/db/project/controllers/rest/CompaniesRestController.java
+++ src/main/java/it/unisalento/db/project/controllers/rest/CompaniesRestController.java
@@ -1,14 +1,12 @@
 package it.unisalento.db.project.controllers.rest;
 
-import it.unisalento.db.project.models.domain.Company;
+import it.unisalento.db.project.exceptions.CompanyNotFoundException;
+import it.unisalento.db.project.models.dto.CompanyDto;
 import it.unisalento.db.project.services.CompanyService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.repository.query.Param;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @CrossOrigin
 @RestController
@@ -18,7 +16,12 @@ public class CompaniesRestController {
     @Autowired private CompanyService companyService;
 
     @GetMapping
-    public Page<Company> getAllCompanies(@Param("page") Integer page) {
+    public Page<CompanyDto> getAllCompanies(@Param("page") Integer page) {
         return this.companyService.findAll(page);
+    }
+
+    @GetMapping("/{id}")
+    public  CompanyDto getById(@PathVariable("id") String id) throws CompanyNotFoundException {
+        return this.companyService.findById(id);
     }
 }
---
+++ src/main/java/it/unisalento/db/project/models/dto/CompanyDto.java
@@ -0,0 +1,23 @@
+package it.unisalento.db.project.models.dto;
+
+public class CompanyDto {
+
+    private String id;
+    private String name;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
