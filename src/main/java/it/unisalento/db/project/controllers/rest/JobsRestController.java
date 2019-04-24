9148bd1e8f7edd5392de29981aee9aa58c612c41
Leonardo <leonardo.capoccia@hotmail.it>
2019-04-24 11:46:15 +0200

    Using a DTO for returning jobs to frontend

--- src/main/java/it/unisalento/db/project/controllers/rest/JobsRestController.java
+++ src/main/java/it/unisalento/db/project/controllers/rest/JobsRestController.java
@@ -1,6 +1,6 @@
 package it.unisalento.db.project.controllers.rest;
 
-import it.unisalento.db.project.models.domain.Job;
+import it.unisalento.db.project.models.dto.JobDto;
 import it.unisalento.db.project.services.JobService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -19,7 +19,7 @@ public class JobsRestController {
     @Autowired private JobService jobService;
 
     @GetMapping
-    public Page<Job> findJobs(@Param("page") Integer page) {
+    public Page<JobDto> findJobs(@Param("page") Integer page) {
         return this.jobService.getJobs(page);
     }
 
--- src/main/java/it/unisalento/db/project/services/JobService.java
+++ src/main/java/it/unisalento/db/project/services/JobService.java
@@ -1,11 +1,17 @@
 package it.unisalento.db.project.services;
 
 import it.unisalento.db.project.models.domain.Job;
+import it.unisalento.db.project.models.dto.JobDto;
 import it.unisalento.db.project.repository.JobRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 
 @Service
@@ -16,8 +22,23 @@ public class JobService {
     @Autowired
     private JobRepository repository;
 
-    // Page is zero-indexed
-    public Page<Job> getJobs(Integer page) {
-        return this.repository.findAll(PageRequest.of(page, PAGE_SIZE));
+
+    public Page<JobDto> getJobs(Integer page) {
+        Page<Job> jobPage = this.repository.findAll(PageRequest.of(page, PAGE_SIZE));
+
+        List<JobDto> dtos = new ArrayList<>();
+        for (Job j : jobPage.getContent()) {
+            JobDto dto = new JobDto();
+            dto.setId(j.get_id().toString());
+            dto.setFirstVisit(j.getPosted());
+            dto.setLastVisit(j.getHiringDate());
+            dto.setRequirements(j.getRequirements());
+            dto.setResponsibilities(j.getResponsibilities());
+            dto.setLink(j.getLink());
+            dtos.add(dto);
+        }
+
+        return new PageImpl<>(dtos, jobPage.getPageable(), jobPage.getTotalElements());
     }
+
 }
