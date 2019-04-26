3045995602caff48c0937771cb8ff0621912e877
Leonardo <leonardo.capoccia@hotmail.it>
2019-04-26 15:33:07 +0200

    Removed unused imports

--- src/main/java/it/unisalento/db/project/repository/JobRepository.java
+++ src/main/java/it/unisalento/db/project/repository/JobRepository.java
@@ -1,13 +1,11 @@
 package it.unisalento.db.project.repository;
 
 import it.unisalento.db.project.models.domain.Job;
-import it.unisalento.db.project.models.dto.CompanyDto;
 import org.bson.types.ObjectId;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import it.unisalento.db.project.models.domain.Platform;
 import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository("job-repository")
